a17fa9cfcc122cfe651cf522e65db4019d09f1f6
Javier348solis <javiersolisretana@gmail.com>
2024-11-18 14:41:39 -0600

    estilos login

--- full_stack_fe/src/App.jsx
+++ full_stack_fe/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FormRegistro from './pages/FormRegistro'
+import Formlogin from './pages/Formlogin'
 
 function App() {
  
 
   return (
     <>
-    <FormRegistro/>
+   <Formlogin/>
     </>
   )
 }
--- full_stack_fe/src/pages/Formlogin.jsx
+++ full_stack_fe/src/pages/Formlogin.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import '../styles/Login'
+import { useState, useEffect } from 'react'
+import '../styles/Login.css'
 
-function Formlogin() {
-  return (
-    <div>
-      <input type="text" placeholder='Nombre Completo'/>
-      <input type="email" placeholder='Correo electronico'/>
-      <input type="password" placeholder='Contraseña'/>
-      <button type="submit">Ingresar</button>
-    </div>
-  )
-}
 
-export default Formlogin
+function FormLogin() {
+
+
+
+    return (
+      <div className="login-container">
+        <div className="login-form-container">
+          <h1>Inicio de Sesión</h1>
+          <input type="text" placeholder="Nombre Completo" />
+          <input type="email" placeholder="Correo electrónico" />
+          <input type="password" placeholder="Contraseña" />
+          <button type="submit">Ingresar</button>
+          <a href="">No tienes una cuenta? Regístrate aquí</a>
+          <a href="">¿Olvidaste tu contraseña?</a>
+        </div>
+      </div>
+    );
+  }
+
+export default FormLogin
