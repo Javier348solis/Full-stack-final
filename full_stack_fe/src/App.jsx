7ead68d99c3b245337cdac65a1a71a874aead02d
Javier348solis <javiersolisretana@gmail.com>
2024-12-10 10:43:56 -0600

    Cambios en la importaciones (app)

--- full_stack_fe/src/App.jsx
+++ full_stack_fe/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import FormLogin from './pages/Formlogin'
-import FormRegistro from './pages/FormRegistro'
-import Home from './pages/Home'
-import Routing from './Rutas/Routing'
 
 
 
